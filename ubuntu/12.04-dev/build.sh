ddd63c3daa3838d81a3a37d1a8987a8ede271f47
tsaikd <tsaikd@gmail.com>
2014-01-13 18:00:20 +0800

    add ROOT_PASSWD in config.sh.sample

--- ubuntu/12.04-dev/build.sh
+++ ubuntu/12.04-dev/build.sh
@@ -4,17 +4,18 @@
 # iputils-ping for ping
 # vim for vi
 # psmisc for killall
+# lsb-release for lsb_release
 # wget for wget
 # git for git
 # openssh-server for sshd
 apt-get -q update || exit $?
 apt-get -q -y dist-upgrade || exit $?
-apt-get -q -y install net-tools iputils-ping vim wget psmisc git openssh-server || exit $?
+apt-get -q -y install net-tools iputils-ping vim wget psmisc lsb-release git openssh-server || exit $?
 apt-get -q  clean || exit $?
 
 # prepare ssh server
 mkdir -p /var/run/sshd
-chpasswd <<<"root:admin123"
+chpasswd <<<"root:${ROOT_PASSWD}"
 
 # install tsaikd bash
 git clone https://github.com/tsaikd/bash "${HOME}/.my-shell" || exit $?
