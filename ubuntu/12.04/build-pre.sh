b85ae76123a491c66b23aa253dbfd6dfb8ad093c
tsaikd <tsaikd@gmail.com>
2014-01-25 23:18:32 +0800

    add custom files in build-pre, start-post

--- ubuntu/12.04/build-pre.sh
+++ ubuntu/12.04/build-pre.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ -d "$DOCKER_SRC/root" ] ; then
+	cp -aL "$DOCKER_SRC/root/"* /
+fi
+
 true
 
--- ubuntu/12.04/start-post.sh
+++ ubuntu/12.04/start-post.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -d "$DOCKER_SRC/custom" ] ; then
+	cp -aL "$DOCKER_SRC/custom/"* /
+fi
+
 bash
 
 true
