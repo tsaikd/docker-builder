c86f474ffdaa94e01c3a86ae9f6be17dc33b84d7
tsaikd <tsaikd@gmail.com>
2014-02-08 21:49:18 +0800

    add ubuntu/phpvirtualbox-4.3.1

---
+++ ubuntu/phpvirtualbox-4.3.1/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get -q update || exit $?
+apt-get -q -y install p7zip-full || exit $?
+apt-get -q clean || exit $?
+
+# phpvirtualbox 4.3-1 tarball http://sourceforge.net/projects/phpvirtualbox/files/phpvirtualbox-4.3-1.zip/download
+cd /tmp || exit $?
+7z x "$DOCKER_SRC/phpvirtualbox-4.3-1.zip" || exit $?
+mv phpvirtualbox-4.3-1/* /var/www/ || exit $?
+rmdir phpvirtualbox-4.3-1 || exit $?
+
+cd /var/www || exit $?
+cp -a config.php-example config.php || exit $?
+sed -i "s/^\(var \$username = '\)\(\S*\)'/\1${PHPVIRTUALBOX_RUN_USER}'/" config.php || exit $?
+sed -i "s/^\(var \$password = '\)\(\S*\)'/\1${PHPVIRTUALBOX_RUN_PASS}'/" config.php || exit $?
+sed -i "s|^\(var \$location = '\)\(\S*\)'|\1${PHPVIRTUALBOX_VBOXWEBSRV_URL}'|" config.php || exit $?
+
