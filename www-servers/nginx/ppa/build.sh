cfce2a35c85db2928447d58aedae1b9428d5cb81
tsaikd <tsaikd@gmail.com>
2014-08-14 17:32:37 +0800

    fix www-servers/nginx/ppa/build.sh

--- www-servers/nginx/ppa/build.sh
+++ www-servers/nginx/ppa/build.sh
@@ -7,5 +7,7 @@ deb http://ppa.launchpad.net/nginx/stable/ubuntu ${redist} main
 deb-src http://ppa.launchpad.net/nginx/stable/ubuntu ${redist} main
 EOF
 
+apt-get -q update || exit $?
+
 apt-get -q -y --force-yes install nginx || exit $?
 
