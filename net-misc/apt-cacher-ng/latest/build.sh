6789c8abe925f8d1186287eef8cd909bb65edf0e
tsaikd <tsaikd@gmail.com>
2014-08-19 17:06:17 +0800

    update net-misc/apt-cacher-ng/latest

--- net-misc/apt-cacher-ng/latest/build.sh
+++ net-misc/apt-cacher-ng/latest/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+if [ -d "/var/cache/apt-cacher-ng" ] ; then
+	chown 103:106 -R "/var/cache/apt-cacher-ng" || exit $?
+fi
+
 if ! type apt-cacher-ng &>/dev/null ; then
 	apt-get -q -y install apt-cacher-ng || exit $?
+	if [ "${http_proxy}" ] ; then
+		echo "Proxy: ${http_proxy}" >> /etc/apt-cacher-ng/acng.conf
+	elif [ "${https_proxy}" ] ; then
+		echo "Proxy: ${https_proxy}" >> /etc/apt-cacher-ng/acng.conf
+	fi
 fi
 
 true
--- net-misc/apt-cacher-ng/latest/start.sh
+++ net-misc/apt-cacher-ng/latest/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ -d "/var/cache/apt-cacher-ng" ] ; then
+	chown 103:106 -R "/var/cache/apt-cacher-ng" || exit $?
+fi
+
 service apt-cacher-ng restart
 
