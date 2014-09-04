a7759236f520bd862f94edbd88667c7a5267dd8a
tsaikd <tsaikd@gmail.com>
2014-09-04 13:56:52 +0800

    update dev-lang/golang/1.3.1

--- dev-lang/golang/1.3.1/build.sh
+++ dev-lang/golang/1.3.1/build.sh
@@ -6,6 +6,13 @@ if ! type go &>/dev/null ; then
 	apt-get -q -y install build-essential || exit $?
 
 	tar -C /usr/local -xzf "${DOCKER_SRC}/go${ver}.linux-amd64.tar.gz" || exit $?
+
+	# set GOPATH
+	export GOPATH="${GOPATH:-/opt/go}"
+	export PATH="${GOPATH}/bin:/usr/local/go/bin:${PATH}"
+	echo 'export GOPATH="${GOPATH:-/opt/go}"' >> /etc/profile.d/01-env.sh
+	echo 'export PATH="${GOPATH}/bin:/usr/local/go/bin:${PATH}"' >> /etc/profile.d/01-env.sh
+	mkdir -p "${GOPATH}" || exit $?
 fi
 
 true
