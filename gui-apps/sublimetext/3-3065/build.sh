cf28347a7ffbd5d0101d78e0c989402095ef35db
tsaikd <tsaikd@gmail.com>
2014-09-12 09:34:07 +0800

    add gui-apps/sublimetext/3-3065

---
+++ gui-apps/sublimetext/3-3065/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! type subl &>/dev/null ; then
+	apt-get -q -y install libglib2.0-0 libcairo2 libpango1.0-0 libgtk2.0-0 || exit $?
+
+	pushd "${DOCKER_SRC}" &>/dev/null
+	dpkg -i sublime-text*.deb || exit $?
+	popd &>/dev/null
+fi
+
+true
+
