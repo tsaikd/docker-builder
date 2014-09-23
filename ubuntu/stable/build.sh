3e30e73692d9529c14fb83188ce6fa7b7fce1adb
tsaikd <tsaikd@gmail.com>
2014-09-23 18:18:32 +0800

    add more default support language in ubuntu:stable

--- ubuntu/stable/build.sh
+++ ubuntu/stable/build.sh
@@ -6,14 +6,14 @@ if [ "${APT_SITE}" ] ; then
 	echo "deb ${APT_SITE} trusty-security main universe" >> /etc/apt/sources.list
 fi
 
-if [ -f "/usr/share/zoneinfo/${TIMEZONE}" ] ; then
-	echo "${TIMEZONE}" > /etc/timezone
-	dpkg-reconfigure --frontend noninteractive tzdata
-fi
-
-locale-gen "${LANG}"
+locale-gen ${LANG} ${SUPPORT_LANG}
 
 dpkg-reconfigure locales
 
 update-locale LANG="${LANG}"
+
+if [ -f "/usr/share/zoneinfo/${TIMEZONE}" ] ; then
+	echo "${TIMEZONE}" > /etc/timezone
+	dpkg-reconfigure --frontend noninteractive tzdata
+fi
 
