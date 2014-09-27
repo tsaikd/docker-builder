4362cf72f37344c6fa64539b4b19d00e0674716e
tsaikd <tsaikd@gmail.com>
2014-09-27 23:31:11 +0800

    update net-dns/dnsmasq/latest

--- net-dns/dnsmasq/latest/build.sh
+++ net-dns/dnsmasq/latest/build.sh
@@ -5,6 +5,9 @@ if ! type dnsmasq &>/dev/null ; then
 
 	# see https://github.com/docker/docker/issues/1951#issuecomment-36303920
 	echo "user=root" >> /etc/dnsmasq.conf
+
+	# fix for ubuntu resolvconf
+	echo "resolv-file=/etc/resolv.conf" >> /etc/dnsmasq.conf
 fi
 
 true
