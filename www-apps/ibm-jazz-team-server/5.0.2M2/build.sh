0a60aec7a4cd4730316fcc9963a4271be0488549
tsaikd <tsaikd@gmail.com>
2014-10-16 16:34:12 +0800

    www-apps/ibm-jazz-team-server/5.0.2M2: update for proxy env

--- www-apps/ibm-jazz-team-server/5.0.2M2/build.sh
+++ www-apps/ibm-jazz-team-server/5.0.2M2/build.sh
@@ -3,7 +3,7 @@
 if [ ! -d "/opt/IBM/JazzTeamServer/server" ] ; then
 	mkdir -p /opt/IBM/JazzTeamServer
 	pushd /opt/IBM/JazzTeamServer
-	unzip "${DOCKER_SRC}/JTS-CCM-keys-Linux64_5.0.2M2.zip"
+	unzip "${DOCKER_SRC}/JTS-CCM-keys-Linux64_5.0.2M2.zip" >/dev/null
 	popd
 fi
 
--- www-apps/ibm-jazz-team-server/5.0.2M2/test.sh
+++ www-apps/ibm-jazz-team-server/5.0.2M2/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-maxtime="$(( $(date +%s) + 180 ))"
+maxtime="$(( $(date +%s) + 300 ))"
 
 for i in 9443 ; do
 	echo "Testing tcp port ${i} is opened ..."
@@ -11,8 +11,8 @@ for i in 9443 ; do
 done
 
 for i in https://localhost:9443/jts/setup ; do
-	echo "Testing http request ${i} success ..."
-	while [ -z "$(curl -sIk "${i}" | sed -n "1{/200/p}")" ] ; do
+	echo "Testing http request ${i} ..."
+	while [ -z "$(https_proxy="" curl -sIk -m 9 "${i}" | sed -n "1{/200/p}")" ] ; do
 		[ "$(date +%s)" -gt "${maxtime}" ] && exit 1
 		sleep 1
 	done
