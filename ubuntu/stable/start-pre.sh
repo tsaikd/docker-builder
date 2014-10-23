f5a0df544da32644f93078908f561709552d64bd
tsaikd <tsaikd@gmail.com>
2014-10-23 11:30:01 +0800

    add wait_iface function for start.sh

--- ubuntu/stable/start-pre.sh
+++ ubuntu/stable/start-pre.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+function wait_iface() {
+	local iface="${1:-eth0}"
+	local timeout="${2:-60}"
+	local maxtime="$(( $(date +%s) + ${timeout} ))"
+
+	if ! ifconfig "${iface}" >/dev/null 2>&1 ; then
+		echo "Waiting interface '${iface}' ..."
+		sleep 1
+	fi
+
+	while ! ifconfig "${iface}" >/dev/null 2>&1 ; do
+		[ "$(date +%s)" -gt "${maxtime}" ] && return 1
+		sleep 2
+	done
+
+	return 0
+}
+
 if [ -f /config.sh ] ; then
 	source /config.sh
 fi
