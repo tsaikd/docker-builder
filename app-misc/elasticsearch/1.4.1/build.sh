e259da1108da17d086a3af04f91a457afeacc279
tsaikd <tsaikd@gmail.com>
2014-11-28 13:38:28 +0800

    app-misc/elasticsearch/1.4.1: add new package

---
+++ app-misc/elasticsearch/1.4.1/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if ! type elasticsearch &>/dev/null ; then
+	ver="1.4.1"
+	tar -C /usr/local -xzf "${DOCKER_SRC}/elasticsearch-${ver}.tar.gz"
+	ln -s "/usr/local/elasticsearch-${ver}/" "/usr/local/elasticsearch"
+	ln -s /usr/local/elasticsearch/bin/* /usr/local/bin/
+fi
+
