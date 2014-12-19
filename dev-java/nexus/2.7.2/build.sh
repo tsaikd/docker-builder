64e5ddc52876d5ff5213c499c1a385feec6f8d2c
tsaikd <tsaikd@gmail.com>
2014-12-20 02:05:48 +0800

    dev-java/nexus/2.7.2: update test function

--- dev-java/nexus/2.7.2/build.sh
+++ dev-java/nexus/2.7.2/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-ver="2.7.2"
+mv "${DOCKER_SRC}/nexus-${NEXUS_VERSION}.war" /var/lib/tomcat7/webapps
 
-mv "${DOCKER_SRC}/nexus-${ver}.war" /var/lib/tomcat7/webapps
-
-mkdir -p /usr/share/tomcat7/sonatype-work/nexus
-
-chown -R tomcat7:tomcat7 /usr/share/tomcat7/sonatype-work/nexus
+mkdir -p /root/sonatype-work/nexus
 
--- dev-java/nexus/2.7.2/test.sh
+++ dev-java/nexus/2.7.2/test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-url="http://localhost:8080/nexus-2.7.2/"
-echo "Testing url ${url} is valid ..."
-wget -q --spider --no-proxy "${url}"
+maxtime="$(( $(date +%s) + 180 ))"
+
+echo "Testing url http://localhost:8080/nexus-${NEXUS_VERSION}/ is valid ..."
+while [ "$(date +%s)" -lt "${maxtime}" ] ; do
+	if exec 5<> /dev/tcp/localhost/8080 ; then
+		echo -e "GET /nexus-${NEXUS_VERSION}/index.html HTTP/1.0\r\nHost: localhost\r\n\r\n" >&5
+		[ "$(cat <&5 | grep "HTTP/1.1 200 OK")" ] && break
+	fi
+	sleep 1
+done
+
