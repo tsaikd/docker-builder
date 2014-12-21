39a1bd08fe17f2bde555f9d25dcfa5eaca960c28
tsaikd <tsaikd@gmail.com>
2014-12-21 17:55:35 +0800

    dev-java/nexus/2.7.2: change url from /nexus-2.7.2 to /nexus

--- dev-java/nexus/2.7.2/build.sh
+++ dev-java/nexus/2.7.2/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv "${DOCKER_SRC}/nexus-${NEXUS_VERSION}.war" /var/lib/tomcat7/webapps
+mv "${DOCKER_SRC}/nexus-${NEXUS_VERSION}.war" /var/lib/tomcat7/webapps/nexus.war
 
 mkdir -p /root/sonatype-work/nexus
 
--- dev-java/nexus/2.7.2/test.sh
+++ dev-java/nexus/2.7.2/test.sh
@@ -2,10 +2,11 @@
 
 maxtime="$(( $(date +%s) + 180 ))"
 
-echo "Testing url http://localhost:8080/nexus-${NEXUS_VERSION}/ is valid ..."
-while [ "$(date +%s)" -lt "${maxtime}" ] ; do
+echo "Testing url http://localhost:8080/nexus/ is valid ..."
+while ((1)) ; do
+	[ "$(date +%s)" -gt "${maxtime}" ] && exit 1
 	if exec 5<> /dev/tcp/localhost/8080 ; then
-		echo -e "GET /nexus-${NEXUS_VERSION}/index.html HTTP/1.0\r\nHost: localhost\r\n\r\n" >&5
+		echo -e "GET /nexus/index.html HTTP/1.0\r\nHost: localhost\r\n\r\n" >&5
 		[ "$(cat <&5 | grep "HTTP/1.1 200 OK")" ] && break
 	fi
 	sleep 1
